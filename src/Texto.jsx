7a8e6814c1edb53d5f768c0db4ff8843d904b6cc
Johnkid65 <85648916+Johnkid65@users.noreply.github.com>
2022-10-09 01:47:58 -0300

    song 1

--- src/Texto.jsx
+++ src/Texto.jsx
@@ -5,7 +5,7 @@ function Texto(props) {
     return (
         <div>
                 <p>clic <a href="https://open.spotify.com/track/2rG0suHy1HWApXi9NZhw5o?si=b760b848d4a84441" target="_blank" className="App-a">acá</a> para agregar musica de fondo (recomendado)</p>
-                <p>activa el icono de repetir en la canción, <br/>si lees desde el celular rotálo para poder leer mejor los parrafos.
+                <p>activa el icono de repetir en la canción "Child I WIll Hurt You - Crystal Castles", <br/>si lees desde el celular rotálo para poder leer mejor los parrafos.
                     <br/> and youre all set up.</p>
                
         </div>
